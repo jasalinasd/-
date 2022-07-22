359c4b4f38ccef7f35e27e0cff67df78a205da14
lbetanzo <lbetanzob@gmail.com>
2022-07-22 11:32:22 -0400

    segunda version

--- src/main/java/mavesa/jornada20220722/controlador/BoletaController.java
+++ src/main/java/mavesa/jornada20220722/controlador/BoletaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+//prueba
 @RestController
 @RequestMapping("/ventas")
 public class BoletaController {
