6254bf6bbcc6b8be2f611872781f83718ab0b35b
jorgecc <jcastro@eftec.cl>
2022-07-27 11:57:53 -0400

    27/07 (5)

--- src/main/java/mavesa/jornada20220722/repo/ISolicitudRepo.java
+++ src/main/java/mavesa/jornada20220722/repo/ISolicitudRepo.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface ISolicitudRepo extends CrudRepository<Solicitud,Integer> {
     @Query("select new mavesa.jornada20220722.valueobject.AgruparClienteVO(S.cliente.nombre,SUM(S.cantidad)) "+
-            "from Solicitud S group by S.cliente.nombre")
+            "from Solicitud S group by S.cliente.nombre order by SUM(S.cantidad) desc")
     public List<AgruparClienteVO> listarAgrupado();
 }
